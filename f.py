54a402610f4d397efda15ab0dbe36013e7d9723e
Uma7036 <113987801+Uma7036@users.noreply.github.com>
2023-05-23 10:50:21 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Create a DataFrame with a single column named 'Name'
+data = {'Name': ['John Doe']}
+df = pd.DataFrame(data)
+
+# Specify the file path to save the Excel file
+file_path = 'output.xlsx'
+
+# Save the DataFrame to Excel
+df.to_excel(file_path, index=False)
+
+print("Name saved to Excel file:", file_path)
